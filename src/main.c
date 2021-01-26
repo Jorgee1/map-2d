84e101e8df1a134ed480e8063eb2a49686c26365
Jorgee1 <jorge.ortega.p.1@gmail.com>
2021-01-26 13:34:02 -0500

    Program can now select a tile using the mouse

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include "utils.h"
 #include "sprite.h"
 
+
+
 int main(int argc, char *argv[])
 {
     Colors colors;
@@ -15,7 +17,7 @@ int main(int argc, char *argv[])
     colors.white = (SDL_Color) {0xFF, 0xFF, 0xFF, 0xFF};
     colors.black = (SDL_Color) {0x00, 0x00, 0x00, 0xFF};
 
-    int upscale = 2;
+
     Screen screen;
     int init = init_screen(
         &screen,
@@ -49,56 +51,128 @@ int main(int argc, char *argv[])
         load_tileset(temp_tileset, temp_sheet);
     }
 
+    // Tileset Layout
+    int upscale = 3;
 
-    int nw = 3;
+    int nw = 4;
+    int i_tileset = 0;
+    int n_tiles = tileset[i_tileset].n;
+
+    SDL_Rect tile_rect[n_tiles];
+
+    int carry_y = 0;
+    int tile_selected = -1;
+
+    for (int y = 0; y < n_tiles/2; y++)
+    {
+        int carry_x = 0;
+        for (int x = 0; x < nw; x++)
+        {
+            int index = x + y * nw;
+            if (index < n_tiles)
+            {
+                Sprite *sprite = &tileset[i_tileset].sprites[x + y * nw];
+
+                tile_rect[index].x = x * sprite->rect.w * upscale + carry_x + 10;
+                tile_rect[index].y = y * sprite->rect.h * upscale + carry_y;
+                tile_rect[index].y += spritesheet[i_tileset].rect.h + 10;
+
+                tile_rect[index].w = sprite->rect.w * upscale;
+                tile_rect[index].h = sprite->rect.h * upscale;
+
+                carry_x += 10;
+            }
+        }
+        carry_y += 10;
+    }
+
+
+
+
+
+
+
+
+    SDL_Rect mouse = (SDL_Rect) {0, 0, 10, 10};
+    bool mouse_b = false;
+
     while (screen.exit == SDL_GAME_RUN)
     {
         while (true)
         {
             SDL_Event event;
             if (SDL_PollEvent(&event) == 0) break;
-            if (event.type == SDL_QUIT) screen.exit = SDL_GAME_EXIT;
+
+            if (event.type == SDL_QUIT)
+            {
+                screen.exit = SDL_GAME_EXIT;
+            }
+            else if (event.type == SDL_MOUSEMOTION)
+            {
+                SDL_GetMouseState(&mouse.x, &mouse.y);
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    mouse_b = true;
+                }
+            }
+            else if (event.type == SDL_MOUSEBUTTONUP)
+            {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    mouse_b = false;
+                }
+            }
         }
         set_render_draw_color(screen.renderer, colors.black);
         SDL_RenderClear(screen.renderer);
 
-
         SDL_RenderCopy(
             screen.renderer,
-            spritesheet[0].texture,
+            spritesheet[i_tileset].texture,
             NULL,
-            &spritesheet[0].rect
+            &spritesheet[i_tileset].rect
         );
 
-        int n_tiles = tileset[0].n;
-        int carry_y = 0;
-        for (int y = 0; y < n_tiles/2; y++)
+
+        for (int i = 0; i < n_tiles; i++)
         {
-            int carry_x = 0;
-            for (int x = 0; x < nw; x++)
+            Sprite *sprite = &tileset[i_tileset].sprites[i];
+
+            if (check_collition(mouse, tile_rect[i]) == 1)
             {
-                int index = x + y * nw;
-                if (index < n_tiles)
+                if (mouse_b == true)
                 {
-                    Sprite *sprite = &tileset[0].sprites[x + y * nw];
-                    SDL_Rect rect;
-                    rect.x = x * sprite->rect.w * upscale + carry_x + 10;
-                    rect.y = y * sprite->rect.h * upscale + carry_y;
-                    rect.w = sprite->rect.w * upscale;
-                    rect.h = sprite->rect.h * upscale;
-
-                    rect.y += spritesheet[0].rect.h + 10;
-
-                    SDL_RenderCopy(
-                        screen.renderer,
-                        sprite->texture,
-                        &sprite->rect,
-                        &rect
-                    );
-                    carry_x += 10;
+                    tile_selected = i;
                 }
+                SDL_Rect bound;
+                bound.x = tile_rect[i].x - 5;
+                bound.y = tile_rect[i].y - 5;
+                bound.w = tile_rect[i].w + 10;
+                bound.h = tile_rect[i].h + 10;
+                set_render_draw_color(screen.renderer, colors.white);
+                SDL_RenderFillRect(screen.renderer, &bound);
             }
-            carry_y += 10;
+
+            if (tile_selected == i)
+            {
+                SDL_Rect bound;
+                bound.x = tile_rect[i].x - 5;
+                bound.y = tile_rect[i].y - 5;
+                bound.w = tile_rect[i].w + 10;
+                bound.h = tile_rect[i].h + 10;
+                set_render_draw_color(screen.renderer, colors.red);
+                SDL_RenderFillRect(screen.renderer, &bound);
+            }
+
+            SDL_RenderCopy(
+                screen.renderer,
+                sprite->texture,
+                &sprite->rect,
+                &tile_rect[i]
+            );
         }
         
         SDL_RenderPresent(screen.renderer);
